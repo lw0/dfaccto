5b8d191b650350838eaba97cdd49d5f783dcf675
Lukas Wenzel <lukas.wenzel@hpi.de>
2021-02-18 16:34:46 +0100

    Prepare hdl/ files for new template system

--- hdl/cfg/simple_types.py
+++ hdl/cfg/simple_types.py
@@ -2,12 +2,12 @@ p=Pkg('simple',
       x_templates={'generic/package.vhd': 'pkg_simple.vhd'})
 
 p.Typ('Logic', Simple,
-      x_definition=lambda:'{{>types/logic.part}}')
+      x_definition='{{>types/logic.part}}')
 
 p.Typ('Data', Simple,
       x_width=32,
-      x_definition=lambda:'{{>types/unsigned.part}}')
+      x_definition='{{>types/unsigned.part}}')
 
 p.Typ('Handshake', Complex,
-      x_definition=lambda:'{{>types/handshake.part}}')
+      x_definition='{{>types/handshake.part}}')
 
